cb937b9f883e9aff80273c43f03415351d4d9fc2
FreakinRocket <freakinrocket@gmail.com>
2023-03-15 12:18:31 -0500

    initial commit

---
+++ zjson.go
@@ -0,0 +1,33 @@
+package zjson
+
+// ### CONSTANTS ###
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
+// loads config into config struct from given file name
+func LoadJSON(objectPointer *interface{}, jsonFilePath string) {
+	jsonFile, err := os.Open(jsonFilePath)
+	ChkError(err)
+	defer jsonFile.Close()
+
+	err = json.NewDecoder(jsonFile).Decode(objectPointer)
+	ChkError(err)
+}
+
+// saves config to file from struct
+func SaveJSON(object interface{}, jsonFilePath string) {
+	jsonData, err := json.MarshalIndent(object, "", " ")
+	ChkError(err)
+	err = os.WriteFile(jsonFilePath, jsonData, 0644)
+	ChkError(err)
+}
+
+// saves programming time and log.fatalLn on an error
+func ChkError(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
